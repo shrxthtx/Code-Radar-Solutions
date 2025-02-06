716965b5348fd548380df68ef234dad3b477fd1a
shrxthtx <srishti1540.becse24@chitkara.edu.in>
2025-02-06 17:12:27 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    int s1,s2,s3;
+    scanf("%d%d%d",&s1,&s2,&s3);
+    if(pwr(2)s3=pwr(2)s1+pwr(2)s2)
+    {
+        printf("Valid");
+
+
+    }    
+    else
+    {
+        printf("Invalid");
+    }
+        
+}
